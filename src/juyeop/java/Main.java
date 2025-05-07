a3f51abd9883a0e722fe2b38c842ca1a0b29903c
sheep06031 <sheep06031@gmail.com>
2025-05-08 00:10:04 +0900

    시작 알림 메시지 출력

--- src/juyeop/java/Main.java
+++ src/juyeop/java/Main.java
@@ -3,7 +3,9 @@ package juyeop.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.println("test");
+        System.out.println("== 자바 테스트 게시판 ==");
+        System.out.println("== 프로그램 시작 ==");
+        System.out.println("명령) ");
+        System.out.println("== 프로그램 종료 ==");
     }
 }
