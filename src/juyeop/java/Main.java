2d76055a6c231559c636a12a17f7251865ccee42
sheep06031 <sheep06031@gmail.com>
2025-05-08 00:07:25 +0900

    test

--- src/juyeop/java/Main.java
+++ src/juyeop/java/Main.java
@@ -4,5 +4,6 @@ package juyeop.java;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        System.out.println("test");
     }
 }
